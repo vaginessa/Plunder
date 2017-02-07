a7021a247c304ed791616dc56b98790b8cca6edb
Victor Martinez <knoopx@gmail.com>
2017-02-07 09:15:41 +0100

    Update vlc-android

--- app/src/main/java/com/plunder/plunder/player/PlayerModule.java
+++ app/src/main/java/com/plunder/plunder/player/PlayerModule.java
@@ -1,13 +1,16 @@
 package com.plunder.plunder.player;
 
 import android.app.Application;
+
+import com.plunder.plunder.AppModule;
 import com.plunder.plunder.AppScope;
 import dagger.Module;
 import dagger.Provides;
 import org.videolan.libvlc.LibVLC;
 
-@Module public class PlayerModule {
-  @Provides @AppScope LibVLC provideLibVLC() {
-    return new LibVLC();
+@Module(includes = {AppModule.class})
+public class PlayerModule {
+  @Provides @AppScope LibVLC provideLibVLC(Application app) {
+    return new LibVLC(app.getApplicationContext());
   }
 }
